a16aac13a6cb79b619f41ba396d94b082bdfe68b
宮副大佑 <miyazoedaisuke@miyafukudaisukenoMacBook-Pro.local>
2023-05-29 11:04:28 +0900

    first commit

---
+++ app/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Post
+
+# class PostForm(forms.Form):
+#     title = forms.CharField(max_length=30, label='タイトル')
+#     content = forms.CharField(label='内容', widget=forms.Textarea())
+
+
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ("title", "content",)
+        labels = {
+            "title": "タイトル",
+            "content": "内容",
+        }
+
+        error_messages = {
+            "title": {
+                "required": "タイトルが入力されていません",
+            }
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        a = self.fields
+        for i in self.fields.values():
+            i.widget.attrs["class"] = "form-control mb-3 "
